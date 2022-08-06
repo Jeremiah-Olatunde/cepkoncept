b5e555eae3216c85fd2111888eb2d8b4143036fa
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2022-08-06 15:13:38 +0100

    landing about projects

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,12 @@
 // IMPORT ALL COMPONENT ROOT SCRIPTS
 import "./**/index.ts";
 
+import { Slider } from "~/src/scripts/slider";
+
+
+const slider = new Slider("projects-slider", {
+  focusPosition: "start",
+  animConfig: { duration: .5, easing: [.767, .01, .18, 1.01] }
+});
 
 export {}
