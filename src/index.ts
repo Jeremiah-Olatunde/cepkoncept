ac32717297fd74bd4ba758896a5d5bd942c2356b
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2022-08-20 14:52:38 +0100

    fixed navbar bug and scrolling bug in chrome

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,46 @@ import { scroll } from  "./scripts/scroll";
 
 const links = Array.from(document.querySelectorAll("[data-nav-link]")) as HTMLElement[];
 const sections = Array.from(document.querySelectorAll("[data-nav-target]")) as HTMLElement[];
+
 //--CHANGE ACTIVE NAVBAR LINK BASED ON IN VIEW SECTION---------------
+
+function search<K, T>(
+  k: K, 
+  arr: T[], 
+  compare: (a: T, b: K) => boolean
+): number {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while(start <= end){
+    const pivot = Math.floor((start + end)/2); 
+    [start, end] = compare(arr[pivot], k) ?  [pivot + 1, end] : [start, end - 1];
+  }
+
+  return Math.max(start, end);
+}
+
+const buffer: any[] = [];
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-    const target = (entry.target as HTMLElement).dataset.navTarget;
-    const link = links.find(link => link.dataset.navLink === target);
+    if(entry.isIntersecting) buffer.push(entry);
+    else {
+      const idx = buffer.findIndex(v => v.target === entry.target);
+      if(-1 < idx) buffer.splice(idx, 1);
+    }
+  });
+  
+  const sorted = buffer.slice().sort((a, b) => a.intersectionRatio < b.intersectionRation ? 0 : 1);
+  const link = links.find(link => link.dataset.navLink === sorted.at(-1).target.dataset.navTarget);
 
-    if(!(link instanceof HTMLElement) || !entry.isIntersecting) return;
-
-    links.forEach(link => link.classList.remove("active"));
-    link.classList.add("active");
-  })
+  links.forEach(link => link.classList.remove("active"));
+  link?.classList.add("active");
 }, { threshold: .2 });
 
 sections.forEach(section => observer.observe(section));
 
 //-------------------------------------------------------------------
-
-
 
 //--SCROLLING NAVIGATION FROM LANBAR LINKS---------------------------
 
